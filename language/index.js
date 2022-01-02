ec19e08d112b2f3bd1c6e3fb00a6cee1db444899
Maurício Leite <mauricioleite1@gmail.com>
2022-01-02 10:23:18 -0300

    Texto do Footer adicionado aos idiomas

--- language/index.js
+++ language/index.js
@@ -5,3 +5,14 @@ export const HomeText = {
     en: 'Most played songs',
   }
 }
+
+export const FooterText = {
+  developed: {
+    ptBR: 'Desenvolvido por Maurício Leite.',
+    en: 'Developed by Maurício Leite',
+  },
+  iDoNotOwn: {
+    ptBR: 'Não possuo direitos sobre o conteúdo utilizado aqui. Todo conteúdo é fornecido por ',
+    en: 'Disclaimer: I do not own rights to the content. All content come from ',
+  }
+}
--- redux/userPreferencesSlice.js
+++ redux/userPreferencesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  language: 'en',
+  language: 'ptBR',
 }
 
 const userPreferencesSlice = createSlice({
