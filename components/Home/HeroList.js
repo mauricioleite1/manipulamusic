055ccfe96776cae58b24d366fdf391e305f08728
Maurício Leite <mauricioleite1@gmail.com>
2022-01-02 07:51:14 -0300

    Implementando preview de áudio e funções na lista Hero

--- components/Home/HeroList.js
+++ components/Home/HeroList.js
@@ -20,16 +20,26 @@ const HeroList = ({ data }) => {
       </Top3>
 
       <List>
-        {data && data.tracks.data.map(track => (
-          <Track key={track.id}>
-            <h6>{track.position}</h6>
+        {data && data.tracks.data.map(({ id, link, position, title, artist, preview }) => (
+          <Track key={id}>
+            <h6>{position}</h6>
             <div>
-              <h5>{track.title}</h5>
-              <h6>{track.artist.name}</h6>
+              <h5>{title}</h5>
+              <h6>{artist.name}</h6>
 
             </div>
 
-            <button>Ouvir completa no Deezer</button>
+            <a href={link} target="_blank" rel="noreferrer">
+              <button>Ouvir completa no Deezer</button>
+            </a>
+
+            <audio src={ preview } preload="auto" controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento <code>audio</code>.
+            </audio>
+
+            <button>Favorito</button>
+
           </Track>
         ))}
       </List>
@@ -67,8 +77,8 @@ background: #121FCF;
 `;
 
 const Top3 = styled.div`
-align-items: center;
-justify-content: center;
+  align-items: center;
+  justify-content: center;
   cursor: pointer;
   display: flex;
   flex-flow: row wrap;
@@ -79,29 +89,28 @@ justify-content: center;
 
 const List = styled.ul`
   align-items: center;
-  // justify-content: center;
   height: 20rem;
   display: flex;
   flex-direction column;
   overflow-y: scroll;
   gap: 2px;
   list-style-type: none;
+  margin: auto;
 `;
 
 const Track = styled.li`
   align-items: center;
-  border-radius: 48px;
+  border-radius: 40px;
   display: flex;
   gap: 10px;
   background: linear-gradient(45deg, silver 25%, purple, gold 55%);
   background-color: #4158D0;
-  background-image: linear-gradient(43deg, #6667ab 0%, #6022fb 100%);
-  color: #fff;
-  padding: 10px;
-  width: 50rem;
+  background-image: linear-gradient(43deg, lightgrey 0%, #f4f4f4 100%);
+  padding-inline: 10px;
+  width: 80rem;
 
   h6 {
-    color: lightgrey;
+    color: darkgrey;
     font-weight: 300;
   }
 
